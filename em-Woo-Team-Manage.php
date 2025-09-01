b27388e496541f3d3c9b20508c8b55d7a626a495
Esmond <esmondmccain@gmail.com>
2025-09-01 00:22:11 -0500

    added more detailed comments

--- em-Woo-Team-Manage.php
+++ em-Woo-Team-Manage.php
@@ -1,16 +1,19 @@
 <?php
 
 /**
-* Main plugin file.
-* PHP version 7.4.33
-
-* @category Wordpress_Plugin
-* @package  Esmond-M
-* @author   Esmond Mccain <esmondmccain@gmail.com>
-* @license  https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License
-* @link     esmondmccain.com
-* @return
-*/
+ * Main plugin file for EM WooTeamManage.
+ * 
+ * Handles plugin initialization, text domain loading, and class autoloading.
+ * Registers the main plugin class and ensures minimum PHP version.
+ * 
+ * PHP version 7.4.33
+ *
+ * @category  Wordpress_Plugin
+ * @package   Esmond-M
+ * @author    Esmond Mccain <esmondmccain@gmail.com>
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License
+ * @link      esmondmccain.com
+ */
 
 /**
  * Plugin Name:       EM WooTeamManage
@@ -34,6 +37,10 @@ use emWooTeamManage\init_plugin\Classes\emWooTeamManage;
 
 defined('ABSPATH') or die();
 
+/**
+ * Initializes the EM WooTeamManage plugin.
+ * Loads text domain, main class, and ensures singleton instance.
+ */
 final class emWooTeamManageInit {
 
     const VERSION = '0.1.0';
@@ -41,36 +48,37 @@ final class emWooTeamManageInit {
 
     private static $_instance = null;
 
+    /**
+     * Registers hooks for plugin initialization and class loading.
+     */
     public function __construct() {
-    
-       add_action( 'init', [ $this, 'i18n' ] );        
-       add_action( 'plugins_loaded', [ $this, 'init_class' ] );
- 
+        add_action( 'init', [ $this, 'i18n' ] );        
+        add_action( 'plugins_loaded', [ $this, 'init_class' ] );
     }
 
+    /**
+     * Loads plugin text domain for translations.
+     */
     public function i18n() {
         load_plugin_textdomain( 'emWooTeamManage' );
     }
 
+    /**
+     * Loads the main plugin class.
+     */
     public function init_class() {
         require_once __DIR__ . '/includes/classes/emWooTeamManage.php';
-   
-     
     }
 
-
-
+    /**
+     * Returns the singleton instance of the plugin initializer.
+     */
     public static function get_instance() {
-
         if ( null == self::$_instance ) {
             self::$_instance = new Self();
         }
-
         return self::$_instance;
-
     }
-
 }
-
 
 emWooTeamManageInit::get_instance();
